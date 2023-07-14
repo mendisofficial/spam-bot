24dd1d18819fd69424879328e37ca383f1ce374a
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2023-07-14 18:37:29 +0530

    Update spam.py
    improved the code and fixed some bugs so that the script runs as intended.

--- spam.py
+++ spam.py
@@ -2,22 +2,27 @@ import pyautogui
 import time
 
 msg = input("Enter the message: ")
-n = input("How many times?: ")
+n = int(input("How many times?: "))
+count_down = int(input("set a countdown in seconds: ")
 delay = int(input("Enter the delay: "))
 
 print("Straing the program!")
 
 time.sleep(1)
 
-while(delay != 0):
-	print("T minus " + str(delay) + " seconds")
-	time.sleep(1)
-	delay = delay - 1
+# Use a for loop instead of a while loop for the countdown
+for _ in range(count_down):
+	print(f"Starting in {count_down} second(s))
+ 	count_down -= 1
+  	time.sleep(1)
+  
+
+
 
 print("Spamming!!")
 
-for i in range(0,int(n)):
-	pyautogui.typewrite(msg + "\n")
+for _ in range(0):
+	pyautogui.typewrite(msg + "\n","interval=delay)
 
 print("Done!")
-print("Developed by Chathusha Mendis")
+print("Developed by Chathusha Mendis, forked by chamodhk")
